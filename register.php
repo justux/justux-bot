5cec4149c59dfbd7dbca07f268753348c7738d27
Alberto Giustino <alberto.giustino@artexe.com>
2018-05-08 22:27:12 +0200

    regiter con telegram

--- register.php
+++ register.php
@@ -1,8 +1,8 @@
 <?php
 
 // PARAMETRI DA MODIFICARE
-$WEBHOOK_URL = 'https://{APP NAME}.herokuapp.com/execute.php';
-$BOT_TOKEN = '{TOKEN}';
+$WEBHOOK_URL = 'https://JustuxBot.herokuapp.com/execute.php';
+$BOT_TOKEN = '326644923:AAEuXEILCoQc4dUwVwGmo6ZOxIk12fPjPM0';
 
 // NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
 $API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
